eb6883fc975a5b26e2d4a98326f1fa4d45954679
chinmaygireesh <chinmaygireeshs@gmail.com>
2021-07-19 13:05:10 +0530

    feat:implememts multiple staff application
    -includes multiple staff  case using generic list
    -indroduces display All staf,Delete a staff options in action menu

--- Controller/MenuActions.cs
+++ Controller/MenuActions.cs
@@ -2,41 +2,79 @@ using System;
 using System.Collections.Generic;
 
 namespace staff
-{
+{    
     class MenuActinos
     {   
-        public void AddStaff(Staff staff,int empId)
+        public int EmpId = 0;
+        public void AddStaff(Staff staff,int empId, List<Staff> staffList)
         {
             Console.WriteLine("----EMPLOYEE ENROLLING---");
             staff.Register(empId);
             Console.WriteLine("Staff successfully added..");
+            staffList.Add(staff);
         }
 
-        public void UpdateAStaff(Staff staff)
+        public void DisplayAstaff(List<Staff> staffList)
+        {  
+            int selectedUser = ShowStaffList(staffList);  
+            try 
+            {
+                staffList[selectedUser - 1].Display();
+            }
+            catch(ArgumentOutOfRangeException e)
+            {
+                Console.WriteLine("Exception caught: {0}", e);
+            }
+        }
+
+        public void UpdateAStaff(List<Staff> staffList)
         {                    
+            int selectedUser = ShowStaffList(staffList);  
             try 
             {
-                staff.Update();
-                Console.WriteLine("Updated succesfully...");
+                staffList[selectedUser - 1].Update();
             }
-            catch(System.NullReferenceException e)
+            catch(ArgumentOutOfRangeException e)
             {
-                Console.WriteLine("There is no staff to update:",e);
+                Console.WriteLine("Exception caught: {0}", e);
             }    
         }
 
-        public void DisplayAStaff(Staff staff)
+        public void DisplayAllStaffs(List<Staff> staffList)
+            {   
+                foreach (Staff staff in staffList)  
+                {  
+                    staff.Display();
+                }  
+            }
+
+        public void DeleteAStaff(List<Staff> staffList)
         {
-            try 
+            int selectedUser = ShowStaffList(staffList);
+             try 
             {
-                staff.Display();
+                staffList[selectedUser-1] = null;
+                staffList.RemoveAt(selectedUser-1);
+                GC.Collect();                
             }
-            catch(System.NullReferenceException e)
+            catch(ArgumentOutOfRangeException e)
             {
-                Console.WriteLine("There is no staff to display:",e);
+                Console.WriteLine("Exception caught: {0}", e);
             }    
+        }   
 
-        }
-        
+        public int ShowStaffList(List<Staff> staffList)
+        {
+            int selectedUser;
+            int i = 1;
+            Console.WriteLine("Selecte the Staff");
+            foreach (Staff staff in staffList)  
+            {  
+                Console.WriteLine($"{i}. {staff.Name}");
+                i++;
+            }
+            selectedUser = Convert.ToInt32(Console.ReadLine());
+            return selectedUser;
+        }           
     }
 }
